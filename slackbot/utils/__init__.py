523ab7d197017b1b81a5a1bbf8753b8f9ab1a191
DheerajNalapat <dheeraj@keyvalue.systems>
2025-10-01 18:10:35 +0530

    chore: cleaning up the repo

--- slackbot/utils/__init__.py
+++ slackbot/utils/__init__.py
@@ -2,12 +2,9 @@
 Utilities package for the News Finder Slack Bot.
 """
 
-from .logger import setup_logger, get_logger
 from .reranker import ArticleReranker, RankingConfig, create_article_reranker
 
 __all__ = [
-    "setup_logger",
-    "get_logger",
     "ArticleReranker",
     "RankingConfig",
     "create_article_reranker",
